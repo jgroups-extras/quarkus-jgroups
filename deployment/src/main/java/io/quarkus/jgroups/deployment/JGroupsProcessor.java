c034c6ab3f20547cbd5667ebafa416074808b40a
Bela Ban <belaban@mailbox.org>
2019-06-18 16:43:43 +0200

    ns

--- deployment/src/main/java/io/quarkus/jgroups/deployment/JGroupsProcessor.java
+++ deployment/src/main/java/io/quarkus/jgroups/deployment/JGroupsProcessor.java
@@ -1,12 +1,8 @@
 package io.quarkus.jgroups.deployment;
 
-import io.quarkus.deployment.annotations.BuildStep;
-
-
 /**
  * @author Bela Ban
  */
 public class JGroupsProcessor {
 
-    @BuildStep AdditionalBeanBuildItem
 }
