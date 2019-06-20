a5652bb5b10bc9142277637dc537e97c05db9852
Bela Ban <belaban@mailbox.org>
2019-06-20 09:24:15 +0200

    ns

--- deployment/src/test/java/io/quarkus/jgroups/InjectionTest.java
+++ deployment/src/test/java/io/quarkus/jgroups/InjectionTest.java
@@ -1,6 +1,8 @@
 package io.quarkus.jgroups;
 
-import io.quarkus.test.QuarkusUnitTest;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jgroups.JChannel;
@@ -8,8 +10,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
+import io.quarkus.test.QuarkusUnitTest;
 
 @SuppressWarnings("WeakerAccess")
 public class InjectionTest {
@@ -18,26 +19,44 @@ public class InjectionTest {
     @RegisterExtension
     static final QuarkusUnitTest config = new QuarkusUnitTest()
             .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
-                    .addClasses(BeanUsingJChannel.class)
+                    .addClasses(JChannelBean.class)
                     .addAsResource("application.properties"));
 
     @Inject
-    BeanUsingJChannel beanUsingBare;
+    JChannelBean bean;
 
     @Test
     public void testInjection() {
-        beanUsingBare.verify();
+        bean.verify();
+    }
+
+    @Test
+    public void testConnect() throws Exception {
+        bean.connect().disconnect();
     }
 
     @ApplicationScoped
-    static class BeanUsingJChannel {
+    static class JChannelBean {
 
         @Inject
         JChannel channel;
 
         void verify() {
             Assertions.assertNotNull(channel);
-            assert channel.getView() == null;
+            Assertions.assertAll(() -> channel.isOpen(), () -> Assertions.assertNull(channel.getView()));
+        }
+
+        JChannelBean connect() throws Exception {
+            channel.connect("demo");
+            Assertions.assertTrue(channel.isConnected());
+            Assertions.assertNotNull(channel.getView());
+            return this;
+        }
+
+        JChannelBean disconnect() {
+            channel.disconnect();
+            Assertions.assertFalse(channel.isConnected());
+            return this;
         }
     }
 
